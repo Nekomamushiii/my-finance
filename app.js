7bb2052ae71680d65478a6d40e32713310d93b93
nekomamushii <hurlee.huu132@gmail.com>
2020-07-30 15:10:30 +0800

    hey

--- app.js
+++ app.js
@@ -18,13 +18,51 @@ var uiController = (function () {
     },
   };
 })();
-var financeController = (function () {})();
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+  return {
+    addItem: function (type, desc, value) {
+      var item, id;
+      if (data.items[type].length === 0) id = 1;
+      else id = data.items[type][data.items[type].length - 1].id + 1;
+
+      if (type === "inc") item = new Income(id, desc, value);
+      else item = new Expense(id, desc, value);
+
+      data.items[type].push(item);
+    },
+    seeData: function () {
+      return data;
+    },
+  };
+})();
 var appController = (function (m1, m2) {
   var ctrlAddItem = function () {
     console.log("click hiilee");
     // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
-    console.log(m1.getInput());
+    var input = m1.getInput();
+
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    m2.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
